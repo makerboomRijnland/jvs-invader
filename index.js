f0f29c6d04818d6da85e814117d3a621491917c6
makerboomRijnland <makerboom@mborijnland.nl>
2022-04-23 00:04:30 +0200

    draw in canvas

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-function setup() {}
+// LESSON 1: scope, object, <canvas>
+const game = {
+    canvas: undefined,
 
-window.addEventListener("load", setup);
+    draw: function() {
+        this.context.fillStyle = "#006400";
+        this.context.fillRect(10, 10, 10, 10);
+    },
+    
+    setup: function() {
+        this.canvas = document.getElementById('invader-game');
+        this.context = this.canvas.getContext('2d');
+        this.draw();
+    }
+}
+
+window.addEventListener("load", function() {
+    game.setup();
+});
+
